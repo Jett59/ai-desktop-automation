30be6179b6ff565a1e59a884b1812a6178ccd8b5
Jett Thompson <jett.thompson.59@gmail.com>
2024-11-03 11:01:53 +1100

    add code to invoke a gemini model with a text query

---
+++ src/ai.rs
@@ -0,0 +1,29 @@
+use crate::python;
+
+pub struct AiContext {}
+
+impl AiContext {
+    pub fn new() -> Self {
+        // The API key should be in the GOOGLE_API_KEY environment variable
+        std::env::var("GOOGLE_API_KEY").expect("No GOOGLE_API_KEY found");
+        python::ensure_package_exists("google-generativeai");
+
+        Self {}
+    }
+
+    pub fn text_query(&self, text: &str) -> String {
+        python::run_script(
+            r#"
+import os
+import google.generativeai as genai
+
+GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-1.5-flash')
+result = model.generate_content('{query}')
+print(result.text)
+        "#
+            .replace("{query}", &text.replace("'", "\\'")),
+        )
+    }
+}
